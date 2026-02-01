e2db6e6c0c93a8358597c6278ab315b5f1a98618
krish <krishjaisingh73@gmail.com>
2026-02-01 21:25:34 +0530

    New File

--- Polymorphism.java
+++ Polymorphism.java
@@ -5,14 +5,19 @@ class MathOp {
     double add(double a, double b, double c) {
         return a + b + c;
     }
+    String add(String s1, String s2){
+        return s1+" "+s2;
+    }
 }
 class Polymorphism {
     public static void main(String[] args) {
         int n1 = 5, n2 = 10;
         double a = 1.1, b = 2.3, c = 3.5;
+        String s1 = "Hello", s2 = "World";
 
         MathOp obj = new MathOp();
         System.out.println(obj.add(n1,n2));
         System.out.println(obj.add(a, b, c));
+        System.out.println(obj.add(s1, s2));
     }
 }
