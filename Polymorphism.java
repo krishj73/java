50515896d7bac3cdc567e0f274d5e6d80967ab3e
krish <krishjaisingh73@gmail.com>
2026-02-04 20:05:19 +0530

    New File

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,3 +1,4 @@
+//compile time polymorphism
 class MathOp {
     int add(int n1, int n2) {
         return n1 + n2;
@@ -9,8 +10,21 @@ class MathOp {
         return s1+" "+s2;
     }
 }
+//runtime polymorphism
+class Animal{
+    void sound(){
+        System.out.println("Animal makes sound");
+    }
+}
+class Dog extends Animal{
+    @Override
+    void sound(){
+        System.out.println("Dog barks");
+    }
+}
 class Polymorphism {
     public static void main(String[] args) {
+        //compile time polymorphism
         int n1 = 5, n2 = 10;
         double a = 1.1, b = 2.3, c = 3.5;
         String s1 = "Hello", s2 = "World";
@@ -19,5 +33,15 @@ class Polymorphism {
         System.out.println(obj.add(n1,n2));
         System.out.println(obj.add(a, b, c));
         System.out.println(obj.add(s1, s2));
+
+        //runtime polymorphism
+        Animal a1 = new Dog();
+        a1.sound();
+        
+        /*Animal a[] = [new Dog(), new Cat()]   - array way
+        for (Animal x : a){
+            x.sound()
+        }
+        */
     }
 }
