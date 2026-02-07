c8e482f6e4f94c1e8e7f7e5b3eae3be06518a66c
krish <krishjaisingh73@gmail.com>
2026-02-07 21:47:29 +0530

    New File

---
+++ SingleInheritance.java
@@ -0,0 +1,17 @@
+class A {
+    void name(){
+        System.out.println("hi! my name is A");
+    }
+}
+class B extends A{          //all methods of super/parent class accessible/override-able by sub/child class
+    void work(){
+        System.out.println("i'm a student");
+    }
+}
+class SingleInheritance{
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.name();
+        obj.work();
+    }
+}
